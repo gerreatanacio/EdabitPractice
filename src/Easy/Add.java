a8654ad02e4f5f0e7195c3858c33bda4fe78f80c
Gerrel <gerrel.atanacio@delta.com>
2020-01-14 05:56:56 -0500

    Committing Add Test Case to deltaVDI

---
+++ src/Easy/Add.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sum(1,6);
+	}
+	public static int sum (int a, int b) {
+		return a+b;
+	}
+
+}
